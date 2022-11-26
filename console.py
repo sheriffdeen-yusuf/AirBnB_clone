c63f89d1035c5e056ea1c52182ec204fe29d6594
Raymond Lukwago <lukwagoraymond3@gmail.com>
2022-11-26 18:01:50 +0300

    do all methods

--- console.py
+++ console.py
@@ -25,8 +25,20 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_create(self, line):
-        """Creates a new instance of BaseModel saves
-        it to JSON & prints the id"""
+        """
+        create:
+
+        Creates a new instance of BaseModel saves
+        it to JSON & prints the id
+
+        This method breaks a string of arguments down into smaller chunks,
+        or strings. It will try to return the value of the named attribute.
+        That will be stored, saved, and printed. If the named attribute
+        doesn't exist, KeyError is raised and handled before "** class
+        doesn't exist **" is printed to the screen.
+        If the class name is missing, "** class name missing **" will be
+        printed to the screen.
+        """
         if len(line) == 0:
             print("** class name missing **")
         elif line not in storage.classes():
@@ -37,8 +49,25 @@ class HBNBCommand(cmd.Cmd):
             print(cls.id)
 
     def do_show(self, line):
-        """Prints the string representation of an instance based on the
-        class name and id Ex $ show BaseModel 1234-1234-1234"""
+        """
+
+        show:
+
+        Prints the string representation of an instance based on the
+        class name and id Ex $ show BaseModel 1234-1234-1234
+
+        This method splits the arguments into smaller strings before the
+        arguments are used. If the length of the arguments is less than two,
+        IndexError is raised, handled, and "** instance id missing **" will
+        print to the screen since more than one argument is needed. However,
+        if an appropriate amount of arguments is available, the result of
+        class name and id will print to the screen. Otherwise, KeyError
+        will be raised, handled and "** no instance found **" will be printed
+        to the screen.
+        If the class name is missing, "** class name missing **" will be
+        printed to the screen. If the class name doesn't exist, "** class
+        doesn't exist **" will be printed to the screen.
+        """
         if len(line) == 0:
             print("** class name missing **")
         else:
@@ -55,8 +84,20 @@ class HBNBCommand(cmd.Cmd):
                     print("** no instance found **")
 
     def do_destroy(self, line):
-        """Deletes an instance based on the class name and id
-        save the change into the JSON file"""
+        """
+        destroy:
+
+        Deletes an instance based on the class name and id
+        save the change into the JSON file
+
+        This method requires two arguments, a class name and id. If the class
+        name is missing, "** class name missing **" will be printed to the
+        screen. If class name doesn't exist, "** class doesn't exist **"
+        will be printed to the screen. If class name does exist, then the
+        command will search for the id, or the second argument. If the id is
+        missing, IndexError is raised and "** instance id missing **" will
+        print to the screen. However, if found, the instance will be deleted.
+        """
         if len(line) == 0:
             print("** class name missing **")
         else:
@@ -72,6 +113,29 @@ class HBNBCommand(cmd.Cmd):
                     storage.save()
                 else:
                     print("** no instance found **")
+
+    def do_all(self, line):
+        """
+        all:
+
+        Prints all string representation of all instances based or not on the
+        class name
+
+        This method will print out the string representation of the value
+        of every instance of a class or not a class. If the class doesn't
+        exist, "** class doesn't exist **" will print to the screen.
+        """
+        if len(line) == 0:
+            full_list = [str(obj) for key, obj in storage.all().items()]
+            print(full_list)
+        if len(line) >= 1:
+            wrds = line.split(' ')
+            if wrds[0] not in storage.classes():
+                print("** class doesn't exist **")
+            else:
+                instance_list = [str(obj) for key, obj in storage.all().items()
+                                 if type(obj).__name__ == wrds[0]]
+                print(instance_list)
 
 
 if __name__ == '__main__':

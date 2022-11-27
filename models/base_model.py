608d35db65945b966af1966b9b5fa73154463adf
Raymond Lukwago <lukwagoraymond3@gmail.com>
2022-11-27 18:44:22 +0300

    Test Model

--- models/base_model.py
+++ models/base_model.py
@@ -34,8 +34,8 @@ class BaseModel:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
         """if kwargs:
@@ -64,7 +64,7 @@ class BaseModel:
         """updates the public instance attribute
         updated_at with the current datetime
         """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):

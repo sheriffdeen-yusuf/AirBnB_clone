e4d055989d2d511c64c78d9e6c40f8cdf7790f60
Raymond Lukwago <lukwagoraymond3@gmail.com>
2022-11-27 18:39:12 +0300

    Test Model

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,32 @@ time = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
     """Represents a BaseModel Class
-    Attributes
+    Args:
+        - *args: list of arguments
+        - **kwargs: dict of key-value pair arguments
     """
 
     def __init__(self, *args, **kwargs):
         """Initialisation Method"""
         if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
+        """if kwargs:
             for keys, values in kwargs.items():
                 if keys == "__class__":
                     if keys == "created_at" and type(keys) is str:
@@ -30,7 +50,7 @@ class BaseModel:
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
         if 'id' not in kwargs:
-            models.storage.new(self)
+            models.storage.new(self)"""
 
     def __str__(self):
         """String representation of a class"""

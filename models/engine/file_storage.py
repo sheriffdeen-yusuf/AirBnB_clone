ebef6e393d0bdfab8d3c6db242bf0da81a545016
Raymond Lukwago <lukwagoraymond3@gmail.com>
2022-11-27 18:23:31 +0300

    Updated dictionaries under classes & Attributes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,15 +4,8 @@
 and methods for serialisation and
 deserialization of JSON data"""
 import datetime
-
 import models
 import json
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 
 
 def models_encod_hook(obj):
@@ -68,16 +61,57 @@ class FileStorage:
     def classes(self):
         """Returns a dictionary of valid classes and their references"""
         from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
         class_dict = {
-            'BaseModel': BaseModel}
+            'BaseModel': BaseModel,
+            'User': User,
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Place': Place,
+            'Review': Review}
         return class_dict
-      
+
     def attributes(self):
         """Returns the valid attributes and their types for classname"""
         attributes = {
             "BaseModel":
                 {"id": str,
                  "created_at": datetime.datetime,
-                 "updated_at": datetime.datetime}
+                 "updated_at": datetime.datetime},
+            "User":
+                {"email": str,
+                 "password": str,
+                 "first_name": str,
+                 "last_name": str},
+            "State":
+                {"name": str},
+            "City":
+                {"state_id": str,
+                 "name": str},
+            "Amenity":
+                {"name": str},
+            "Place":
+                {"city_id": str,
+                 "user_id": str,
+                 "name": str,
+                 "description": str,
+                 "number_rooms": int,
+                 "number_bathrooms": int,
+                 "max_guest": int,
+                 "price_by_night": int,
+                 "latitude": float,
+                 "longitude": float,
+                 "amenity_ids": list},
+            "Review":
+                {"Place_id": str,
+                 "user_id": str,
+                 "text": str}
         }
         return attributes

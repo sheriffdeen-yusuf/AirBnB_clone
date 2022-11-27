86d25a8e203aae34807ce39befe34ed05c69afa6
Raymond Lukwago <lukwagoraymond3@gmail.com>
2022-11-27 18:43:09 +0300

    Test Model

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,8 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
 
         """if kwargs:
             for keys, values in kwargs.items():
@@ -64,7 +65,6 @@ class BaseModel:
         updated_at with the current datetime
         """
         self.updated_at = datetime.utcnow()
-        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
